5005935ec71479b31a8496e6a4ba78af13d671f8
AgnesBressan <agnesbressan3838@gmail.com>
2022-11-07 18:14:56 -0300

    codes

--- src/controles.py
+++ src/controles.py
@@ -3,28 +3,46 @@
 import rospy
 import cv2 as cv
 import numpy as np
-from std_msgs.msg import Float64
+from std_msgs.msg import Int32MultiArray
+from geometry_msgs.msg import Twist
 
 class Controles:
 
     def __init__(self):
 
+        self.vel = Twist()
         self.publish_time = 0.05
+        self.angular_z = 0
+        self.linear_x = 0
 
-        self.dados = rospy.Subscriber("/dados", Float64, self.reed, queue_size = 10000)
+        rospy.Subscriber("/dados", Int32MultiArray, self.reed, queue_size = 10)
 
+        # dados recebidos dos snesores
+        self.dados = [-1, -1, -1, -1, -1]
         self.full_left = self.dados[0] 
         self.left = self.dados[1]
         self.center = self.dados[2]
         self.right = self.dados[3]
         self.full_right = self.dados[4]
 
+        # dados enviados para o cmd_vel
+        # pra esquerda angular em z negativo
+
+        self.cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+
         self.timer_pub = rospy.Timer(rospy.Duration(self.publish_time), self.timerCallback)   
 
-    def reed (self):
+    def reed (self, msg):
 
-        if self.full_left == 0 and self.left == 0 and self.full_right == 0 and self.right == 0 and self.center == 1:
-            print("frente")
+        self.dados = msg.data
+        self.full_left = self.dados[0] 
+        self.left = self.dados[1]
+        self.center = self.dados[2]
+        self.right = self.dados[3]
+        self.full_right = self.dados[4]
+
+        if self.full_left == 1 and self.left == 1 and self.full_right == 0 and self.right == 1 and self.center == 1:
+            self.linear_x = 0.1
 
         if self.full_left == 0 and self.left == 0 and self.full_right == 0 and self.right == 0 and self.center == 0:
             print("para")
@@ -38,16 +56,13 @@ class Controles:
 
 
     def timerCallback(self, event):
-        msg = Float64()
-        msg.data = self.vel
-        self.sensor_pub.publish(msg)
+        msg = Twist()
+        msg.angular.z = self.angular_z
+        msg.linear.x = self.linear_x
+        self.cmd_vel_pub.publish(msg)
 
 if __name__ == '__main__':
 
-    try:
-        rospy.init_node("sensores")
-        Controles()
-        rospy.spin()
-
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node("controles")
+    Controles()
+    rospy.spin()
--- src/sensores.py
+++ src/sensores.py
@@ -3,7 +3,11 @@
 import rospy
 import cv2 as cv
 import numpy as np
-from std_msgs.msg import Float64
+from std_msgs.msg import Int32MultiArray
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+bridge = CvBridge()
 
 class Sensores:
 
@@ -12,27 +16,24 @@ class Sensores:
         self.sensores = np.array([-1, -1, -1, -1, -1])
         self.publish_time = 0.05
 
-        self.img_sensor_full_left = rospy.Subscriber("/sensor/camera_full_left/image_raw", Float64, self.imgFullLeftCallback, queue_size = 10000)
+        self.img_sensor_full_left = rospy.Subscriber("/sensor/camera_full_left/image_raw", Image, self.imgFullLeftCallback, queue_size = 10)
+        self.img_sensor_left = rospy.Subscriber("/sensor/camera_left/image_raw", Image, self.imgLeftCallback, queue_size = 10)
+        self.img_sensor_center = rospy.Subscriber("/sensor/camera_center/image_raw", Image, self.imgCenterCallback, queue_size = 10)
+        self.img_sensor_right = rospy.Subscriber("/sensor/camera_right/image_raw", Image, self.imgRightCallback, queue_size = 10)
+        self.img_sensor_full_right = rospy.Subscriber("/sensor/camera_full_right/image_raw", Image, self.imgFullRightCallback, queue_size = 10)   
 
-        self.img_sensor_left = rospy.Subscriber("/sensor/camera_left/image_raw", Float64, self.imgLeftCallback, queue_size = 10000)
-
-        self.img_sensor_center = rospy.Subscriber("/sensor/camera_center/image_raw", Float64, self.imgCenterCallback, queue_size = 10000)
-
-        self.img_sensor_right = rospy.Subscriber("/sensor/camera_right/image_raw", Float64, self.imgRightCallback, queue_size = 10000)
-
-        self.img_sensor_full_right = rospy.Subscriber("/sensor/camera_full_right/image_raw", Float64, self.imgFullRightCallback, queue_size = 10000)   
-
-        self.sensor_pub = rospy.Publisher("/dados", Float64, queue_size=10)
-
+        self.sensor_pub = rospy.Publisher("/dados", Int32MultiArray, queue_size=10)
+        
         self.timer_pub = rospy.Timer(rospy.Duration(self.publish_time), self.timerCallback)   
 
 
-    def imgFullLeftCallback (self, img):
+    def imgFullLeftCallback (self, ros_img):
 
+        img = bridge.imgmsg_to_cv2(ros_img)       
         self.img_full_left = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        self.cor_full_left = cv.mean(self.img_full_left)
+        self.cor_full_left = np.mean(self.img_full_left)
 
-        if (self.cor_full_left < 50):
+        if (self.cor_full_left < 150):
             self.sensores[0] = 0
 
         elif (self.cor_full_left > 230):
@@ -41,12 +42,13 @@ class Sensores:
         else:
             rospy.logwarn("Cor identificada diferente de branco e preto!\n")
 
-    def imgLeftCallback (self, img):
+    def imgLeftCallback (self, ros_img):
 
+        img = bridge.imgmsg_to_cv2(ros_img)
         self.img_left = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        self.cor_left = cv.mean(self.img_left)
+        self.cor_left = np.mean(self.img_left)
 
-        if (self.cor_left < 50):
+        if (self.cor_left < 150):
            self.sensores[1] = 0
 
         elif (self.cor_left > 230):
@@ -55,50 +57,53 @@ class Sensores:
         else:
             rospy.logwarn("Cor identificada diferente de branco e preto!\n")
 
-    def imgCenterCallback (self, img):
+    def imgCenterCallback (self, ros_img):
 
+        img = bridge.imgmsg_to_cv2(ros_img)
         self.img_center = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        self.cor_center = cv.mean(self.img_center)
+        self.cor_center = np.mean(self.img_center)
 
-        if (self.cor_center < 50):
-            self.sensores[3] = 0
+        if (self.cor_center < 150):
+            self.sensores[2] = 0
 
         elif (self.cor_center > 230):
+            self.sensores[2] = 1
+        
+        else:
+            rospy.logwarn("Cor identificada diferente de branco e preto!\n")
+        
+    def imgRightCallback (self, ros_img):
+
+        img = bridge.imgmsg_to_cv2(ros_img)
+        self.img_right = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        self.cor_right = np.mean(self.img_right)
+
+        if (self.cor_right < 150):
+            self.sensores[3] = 0
+
+        elif (self.cor_right > 230):
             self.sensores[3] = 1
         
         else:
             rospy.logwarn("Cor identificada diferente de branco e preto!\n")
-        
-    def imgRightCallback (self, img):
 
-        self.img_right = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        self.cor_right = cv.mean(self.img_right)
+    def imgFullRightCallback (self, ros_img):
 
-        if (self.cor_right < 50):
+        img = bridge.imgmsg_to_cv2(ros_img)
+        self.img_full_right = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        self.cor_full_right = np.mean(self.img_full_right)
+
+        if (self.cor_full_right < 150):
             self.sensores[4] = 0
 
-        elif (self.cor_right > 230):
-            self.sensores[4] = 1
-        
-        else:
-            rospy.logwarn("Cor identificada diferente de branco e preto!\n")
-
-    def imgFullRightCallback (self, img):
-
-        self.img_full_right = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        self.cor_full_right = cv.mean(self.img_full_right)
-
-        if (self.cor_full_right < 50):
-            self.sensores[5] = 0
-
         elif (self.cor_full_right > 230):
-            self.sensores[5] = 1
+            self.sensores[4] = 1
         
         else:
             rospy.logwarn("Cor identificada diferente de branco e preto!\n")        
 
     def timerCallback(self, event):
-        msg = Float64()
+        msg = Int32MultiArray()
         msg.data = self.sensores
         self.sensor_pub.publish(msg)
 
